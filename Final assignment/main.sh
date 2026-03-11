abfb7ff3d0c1f371709416dd343212327f277ed0
Giulio Ginami <giulioginami@gmail.com>
2026-03-11 20:51:16 +0100

    Adjusted learning rate and dice loss

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
-    --lr 0.001 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "dice-loss" \
+    --experiment-id "lr+dice-loss" \
